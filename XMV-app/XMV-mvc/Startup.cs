c2780e017a7d2e55e135f0865e65e7b476ee54f8
Quattro-Bajeena <motmen001@gmail.com>
2021-07-16 00:26:40 +0200

    basic MVC project setup, created base html structure

--- XMV-app/XMV-mvc/Startup.cs
+++ XMV-app/XMV-mvc/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,7 +24,11 @@ namespace XMV_mvc
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
+			services.AddDbContext<Data.ApplicationDbContext>(options =>
+				options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))
+			);
 			services.AddControllersWithViews().AddRazorRuntimeCompilation();
+			services.AddRazorPages();
 		
 		}
 
